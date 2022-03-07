144550c3549a9d5c46cb8c3dfc3035c7ca36cbf3
xuechenghan <20030lisa@gmail.com>
2022-03-07 12:00:29 +0800

    12

--- B10956250/Program.cs
+++ B10956250/Program.cs
@@ -7,6 +7,23 @@ namespace B10956250
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string test = "test";
+            var aaa = "test";
+            const string tsetconst = "aaa";
+            //testconst = "bb;
+            int price = 100;
+            bool isHappy = true;
+            Console.WriteLine(!isHappy);
+
+
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            int a;
+            a = (int)(meal_cost + tip_percent+tax_percent);
+            Console.WriteLine("總額"+a)
+
         }
+
     }
 }
